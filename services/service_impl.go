ccb3e3ac60485620e060f89e4c3a5a6d1ccb2601
Fabio Martins <fabio.martins@marisapartner.com.br>
2021-05-24 13:01:36 -0300

    Added implemention for repository use

--- services/service_impl.go
+++ services/service_impl.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
@@ -18,13 +19,21 @@ func NewCryptoService(repo repository.CryptoRepository) CryptoService {
 	return &cryptoServiceImpl{repo}
 }
 
-func (s *cryptoServiceImpl) GetLastSymbolsPrice(fsyms, tsyms []string) (types.PricesResponse, error) {
-	response, err := s.requestLastSymbolsPrice(fsyms, tsyms)
-	if err != nil {
-		return response, err
+func (s *cryptoServiceImpl) GetLastSymbolsPrice(fsyms, tsyms []string) (prices types.PricesResponse, err error) {
+	prices, err = s.requestLastSymbolsPrice(fsyms, tsyms)
+	if err == nil {
+		return prices, err
 	}
-	return response, nil
+	priceRows, err := s.repo.GetLastPrice(fsyms, tsyms)
+	if err != nil {
+		return prices, err
+	}
+	if len(priceRows) != 0 {
+		return s.transformCryptoModel(priceRows), err
+	}
+	return prices, errors.New("price data could not be retrieved")
 }
+
 func (s *cryptoServiceImpl) RefreshLastSymbolsPrices(fsyms, tsyms []string) error {
 	return nil
 }
@@ -40,4 +49,47 @@ func (s *cryptoServiceImpl) requestLastSymbolsPrice(fsyms, tsyms []string) (type
 	}
 	err = json.NewDecoder(resp.Body).Decode(&prices)
 	return prices, err
+}
+
+func (s *cryptoServiceImpl) transformCryptoModel(models []types.LastPriceModel) types.PricesResponse {
+	prices := types.PricesResponse{}
+	for _, model := range models {
+		rawpair, ok := prices.RAW[model.FROMSYMBOLDISPLAY]
+		if !ok {
+			rawpair = make(map[string]types.PriceRaw)
+		}
+		rawpair[model.TOSYMBOLDISPLAY] = types.PriceRaw{
+			CHANGE24HOUR:    model.CHANGE24HOUR,
+			CHANGEPCT24HOUR: model.CHANGEPCT24HOUR,
+			OPEN24HOUR:      model.OPEN24HOUR,
+			VOLUME24HOUR:    model.VOLUME24HOUR,
+			VOLUME24HOURTO:  model.VOLUME24HOURTO,
+			LOW24HOUR:       model.LOW24HOUR,
+			HIGH24HOUR:      model.HIGH24HOUR,
+			PRICE:           model.PRICE,
+			LASTUPDATE:      model.LASTUPDATE,
+			SUPPLY:          model.SUPPLY,
+			MKTCAP:          model.MKTCAP,
+		}
+		displaypair, ok := prices.DISPLAY[model.FROMSYMBOLDISPLAY]
+		if !ok {
+			displaypair = make(map[string]types.PriceDisplay)
+		}
+		displaypair[model.TOSYMBOLDISPLAY] = types.PriceDisplay{
+			CHANGE24HOUR:    fmt.Sprintf("%s %.2f", model.TOSYMBOLDISPLAY, model.CHANGE24HOUR),
+			CHANGEPCT24HOUR: fmt.Sprintf("%.2f", model.CHANGEPCT24HOUR),
+			OPEN24HOUR:      fmt.Sprintf("%s %.2f", model.TOSYMBOLDISPLAY, model.OPEN24HOUR),
+			VOLUME24HOUR:    fmt.Sprintf("%s %.2f", model.FROMSYMBOLDISPLAY, model.VOLUME24HOUR),
+			VOLUME24HOURTO:  fmt.Sprintf("%s %.2f", model.TOSYMBOLDISPLAY, model.VOLUME24HOURTO),
+			LOW24HOUR:       fmt.Sprintf("%s %.2f", model.TOSYMBOLDISPLAY, model.LOW24HOUR),
+			HIGH24HOUR:      fmt.Sprintf("%s %.2f", model.TOSYMBOLDISPLAY, model.HIGH24HOUR),
+			PRICE:           fmt.Sprintf("%s %.2f", model.TOSYMBOLDISPLAY, model.PRICE),
+			FROMSYMBOL:      model.FROMSYMBOLDISPLAY,
+			TOSYMBOL:        model.TOSYMBOLDISPLAY,
+			LASTUPDATE:      fmt.Sprint(model.LASTUPDATE),
+			SUPPLY:          fmt.Sprintf("%s %d", model.FROMSYMBOLDISPLAY, model.SUPPLY),
+			MKTCAP:          fmt.Sprintf("%s %.2f", model.TOSYMBOLDISPLAY, model.MKTCAP),
+		}
+	}
+	return prices
 }
