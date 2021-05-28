c5d8fdc208bd0914147395b31f427e96b942aa50
Ferkze <ferkemartins@gmail.com>
2021-05-28 10:42:30 -0300

    Added wiring in main func

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/ferkze/cryptocompare/handler"
+	"github.com/ferkze/cryptocompare/repository/mysql"
+	"github.com/ferkze/cryptocompare/services"
+	"github.com/gorilla/mux"
+)
+
+
+func main() {
+	repo := mysql.NewCryptoRepository(os.Getenv("DB_DSN"))
+	s := services.NewCryptoService(repo)
+	
+	h := handler.NewCryptoHandler(repo,s)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/prices", h.GetPairPrices)
+
+	port := os.Getenv("port")
+	if port == "" {
+		port = "8000"
+		log.Printf("Using port %s as default\n", port)
+	}
+	log.Printf("API running on endpoint: http://localhost:%s/api/prices\n", port)
+	http.ListenAndServe(":"+port, r)
 }
